2e0beb94f329ccd3c2d6ac35c57c534fd9b19d4a
shubhu014 <95476722+shubhu014@users.noreply.github.com>
2022-01-28 18:03:16 +0530

    Create test2801-recipe.rb

---
+++ test2801-recipe.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook:: test2801-cookbook
+# Recipe:: test2801-recipe
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+
+file '/test2801file' do
+    content 'test2801file has been created by chef using jenkins'
+    action :create
+    end
+
+
+package 'apache2' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  content 'Welcome to Data Geek. This is coming from test2801-recipe using jenkins'
+  action :create
+end
+
+execute "run a script" do
+      command <<-EOH
+      service apache2 reload
+      EOH
+end
